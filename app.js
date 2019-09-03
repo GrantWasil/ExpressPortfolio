8b374d162b107974faeeef18a5d7f4ed1fb27cd4
GrantWasil <grantwasil1@gmail.com>
2019-09-02 21:21:56 -0600

    create a working home, about, and error page. organized files

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+app.use("/static", express.static("public"));
+
+app.set("view engine", "pug");
+
+const mainRoutes = require("./routes");
+
+app.use(mainRoutes);
+
+app.use((req, res, next) => {
+  const err = new Error("not found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  console.log("error status: ", err.status);
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(3000, () => {
+  console.log("The app is running on localhost:3000");
+});
---
+++ routes/projects.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const router = express.Rotuer;
+const {projects} = require("../data.json");
